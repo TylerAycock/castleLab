9b1df01fc4e8d00424e47450fe3d57660bfc02a4
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-23 12:17:26 -0700

    added an error on purpose

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ const cors = require(`cors`)
 const path = require(`path`)
 
 
+
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
+
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: 'bd21eec9b3944ae2ad8ea506cc9b5983',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+
+rollbar.log('Hello world!')
 
 console.log(__dirname, '../public/index.html') //__dirname points to where we are in our directory 
 
@@ -23,6 +35,9 @@ app.get(`/profile`, (req,resp)=>{
     resp.status(200).sendFile(path.join(__dirname, '../public/profile.html'))
 })
 
-
+try {nonExistentFunction ()
+} catch(error){
+    console.error(error)
+}
 
 app.listen(4000, console.log(`Server running on 4000!`))
